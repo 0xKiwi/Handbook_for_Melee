ed6424765e9dcbfcea6c55b6524fa5f069b6fac4
thatkawaiiguy <ivanthegreat15@gmail.com>
2016-01-21 10:10:24 -0500

    Prepare for open source more, add GPL headings

--- src/main/java/com/thatkawaiiguy/meleehandbook/adapter/fragment/TabFragmentAdapter.java
+++ src/main/java/com/thatkawaiiguy/meleehandbook/adapter/fragment/TabFragmentAdapter.java
@@ -1,10 +1,27 @@
+/*
+    This file is part of Melee Handbook.
+
+    Melee Handbook is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Melee Handbook is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Melee Handbook.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 package com.thatkawaiiguy.meleehandbook.adapter.fragment;
 
-        import android.support.v4.app.Fragment;
-        import android.support.v4.app.FragmentManager;
-        import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 
-        import com.thatkawaiiguy.meleehandbook.fragment.TechInfoFragment;
+import com.thatkawaiiguy.meleehandbook.fragment.TechInfoFragment;
 
 public class TabFragmentAdapter extends FragmentPagerAdapter {
     private final String[] tabTitles;
@@ -21,11 +38,11 @@ public class TabFragmentAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        if (position == 0)
+        if(position == 0)
             return TechInfoFragment.newInstance(0);
-        else if (position == 1)
+        else if(position == 1)
             return TechInfoFragment.newInstance(1);
-        else if (position == 2)
+        else if(position == 2)
             return TechInfoFragment.newInstance(2);
         return TechInfoFragment.newInstance(position + 1);
     }
